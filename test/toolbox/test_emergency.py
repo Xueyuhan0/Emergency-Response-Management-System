51fa64352276aefbc0e1498286f7dce0d4d38367
Xueyuhan0 <2062642171@qq.com>
2024-06-26 16:32:24 +0800

    2

--- test/toolbox/test_emergency.py
+++ test/toolbox/test_emergency.py
@@ -1,3 +1,13 @@
+import os 
+import sys
+# 获取当前脚本文件的目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 将上级目录（项目根目录）添加到系统路径中
+project_dir = os.path.dirname(os.path.dirname(current_dir))
+sys.path.append(project_dir)
+
+
 from group4.toolbox.emergency import Emergency
 import unittest
 from group4.toolbox.emergency import EmergencyType
